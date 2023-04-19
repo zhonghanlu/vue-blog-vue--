4ab0841560b1f5748ef2ec868599f7d1e93daaf5
zhonghanlu <1420865757@qq.com>
2023-04-20 00:22:28 +0800

    Improvement

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios'
+
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+
+Vue.use(Element)
+
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
